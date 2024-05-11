8fe0a3ae1fdfe75b7f779834d0f52a4664f12a1a
Nour Muhammad <101945184+NourMuhammad10@users.noreply.github.com>
2024-05-11 18:28:23 +0300

    Edit to human play move

--- Human.py
+++ Human.py
@@ -7,13 +7,13 @@ class Human(Player.Player):
         super().__init__(name.capitalize(), color)
 
     def play_move(self, board):
-        print("Enter your next move: ")
+        print("Enter your move: ")
         x = int(input("Enter row: "))
         y = int(input("Enter column: "))
         cell = Cell.Cell(x, y, self.color)
         valid_moves = board.valid_moves(self)
         while cell not in valid_moves:
-            print("Please re-enter a valid move: ", end="")
+            print("Please re-enter a valid move: ")
             x = int(input("Enter row: "))
             y = int(input("Enter column: "))
             cell = Cell.Cell(x, y, self.color)
